40e5d0eaff3109be0417570858d88f6ac5e7b5da
gustavo1 <gf9068586@gmail.com>
2021-11-08 16:16:06 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        yellow:{
+          200:'#808000'
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
